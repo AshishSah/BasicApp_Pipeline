28b8a7889ce220185e5d47ee83fdc41c35158d77
AshishSah <sah.ashish89@gmail.com>
2024-04-03 11:46:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BasicApp_Pipeline;
+
+StartProgram sp = new();
+
+
+int a = 1; int b = 2; string c = "3"; string d = "4";
+
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Sum of {0}, {1} :", d, c);
+sp.Sum(d,c);
+
+Console.WriteLine("Sum of {0}, {1}, {2} :", a, b, -7);
+Console.WriteLine(sp.Sum(a,b, -7));
+
+
+Console.ReadLine();
+
+
+
---
+++ StartProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicApp_Pipeline
+{
+    internal class StartProgram
+    {
+        public void Sum(string a, string b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public int Sum(int a, int b, int c)
+        {
+            return (a + b + c);
+        }
+    }
+}
